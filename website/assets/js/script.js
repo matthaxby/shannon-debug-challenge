75ae91b10754942bda1fc7db2907798c1bd0c311
Shannon Forsman <sforsman1@gmail.com>
2015-04-24 10:38:38 -0600

    it's broken

--- website/assets/js/script.js
+++ website/assets/js/script.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
-	
-	
-	$('.button').click(function() {
-		
+
+
+	$('.button').click(function( {
+
 		$('.programming_career').css('display', 'none');
 		$('.gif').css('display', 'block');
 		$('.gif img').attr("src", "http://i.imgur.com/xilCM.gif" + new Date().getTime());
-		
-		
+
+
 	});
-	
+
 	$('.close').click(function(){
 		$('.programming_career').fadeIn('slow');
 		$('.gif').css('display', 'none');
-		
+
 
 	});
-	
+
 });
-
-
